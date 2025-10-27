270900b1db52e4045073d10ec4260d24909bfcd3
Lily Skye <git@suchipi.tv>
2025-10-26 19:32:17 -0600

    kris

--- src/Player.ts
+++ src/Player.ts
@@ -11,18 +11,21 @@ import {
 } from "@hex-engine/2d";
 
 import placeholder from "./characters/placeholder.aseprite";
+import krisLightWorld from "./characters/kris-lightworld.aseprite";
 
 export default function Player(initialPosition: Vector) {
   useType(Player);
 
   const position = initialPosition.clone();
 
-  const aseprite = useNewComponent(() => Aseprite(placeholder));
+  const sprite = useNewComponent(() => Aseprite(krisLightWorld));
+  sprite.currentAnim = sprite.animations["down-idle"];
+  sprite.currentAnim.play();
 
   const geometry = useNewComponent(() =>
     Geometry({
       shape: Polygon.rectangle(
-        new Vector(aseprite.data.width, aseprite.data.height)
+        new Vector(sprite.data.width, sprite.data.height)
       ),
       position,
     })
@@ -30,7 +33,7 @@ export default function Player(initialPosition: Vector) {
 
   const keyboard = useNewComponent(Keyboard);
 
-  const speed = 0.3;
+  const speed = 0.15;
 
   let movementVector = new Vector(0, 0);
   useUpdate((delta) => {
@@ -38,30 +41,75 @@ export default function Player(initialPosition: Vector) {
     movementVector.y = 0;
 
     if (keyboard.pressed.has("ArrowDown")) {
-      position.addYMutate(1);
+      movementVector.addYMutate(1);
     }
     if (keyboard.pressed.has("ArrowUp")) {
-      position.subtractYMutate(1);
+      movementVector.subtractYMutate(1);
     }
     if (keyboard.pressed.has("ArrowLeft")) {
-      position.subtractXMutate(1);
+      movementVector.subtractXMutate(1);
     }
     if (keyboard.pressed.has("ArrowRight")) {
-      position.addXMutate(1);
+      movementVector.addXMutate(1);
     }
 
     if (movementVector.x !== 0 || movementVector.y !== 0) {
       const magnitude = Math.floor(speed * delta);
-      movementVector.normalizeMutate().multiplyMutate(magnitude);
+      movementVector.multiplyMutate(magnitude);
       position.addMutate(movementVector);
 
       // keep pixel-aligned
       position.roundDownMutate();
     }
+
+    const animBefore = sprite.currentAnim;
+    switch (true) {
+      case movementVector.y < 0: {
+        sprite.currentAnim = sprite.animations["up-walk"];
+        break;
+      }
+      case movementVector.y > 0: {
+        sprite.currentAnim = sprite.animations["down-walk"];
+        break;
+      }
+      case movementVector.x < 0: {
+        sprite.currentAnim = sprite.animations["left-walk"];
+        break;
+      }
+      case movementVector.x > 0: {
+        sprite.currentAnim = sprite.animations["right-walk"];
+        break;
+      }
+      default: {
+        switch (animBefore) {
+          case sprite.animations["up-walk"]: {
+            sprite.currentAnim = sprite.animations["up-idle"];
+            break;
+          }
+          case sprite.animations["down-walk"]: {
+            sprite.currentAnim = sprite.animations["down-idle"];
+            break;
+          }
+          case sprite.animations["left-walk"]: {
+            sprite.currentAnim = sprite.animations["left-idle"];
+            break;
+          }
+          case sprite.animations["right-walk"]: {
+            sprite.currentAnim = sprite.animations["right-idle"];
+            break;
+          }
+        }
+      }
+    }
+
+    if (sprite.currentAnim !== animBefore) {
+      animBefore.pause();
+      sprite.currentAnim.play();
+    }
   });
 
   useDraw((context) => {
-    aseprite.draw(context);
+    sprite.draw(context);
   });
 
   return {
