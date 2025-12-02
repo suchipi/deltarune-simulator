a2b386d23b94bb1a4327c8c1a1bd2517edf3b0dc
Lily Skye <me@suchipi.com>
2025-12-02 14:47:40 -0700

    wip sensors for room transitions

---
+++ src/Sensor.ts
@@ -0,0 +1,66 @@
+import {
+  useType,
+  useNewComponent,
+  Geometry,
+  Polygon,
+  Vector,
+  Physics,
+  useChild,
+} from "@hex-engine/2d";
+
+export type SensorCallbacks = {
+  onStart?: (info: Physics.CollisionEventInfo) => void;
+  onEnd?: (info: Physics.CollisionEventInfo) => void;
+};
+
+export default function Sensor(
+  position: Vector,
+  size: Vector,
+  callbacks: SensorCallbacks
+) {
+  useType(Sensor);
+
+  const geometry = useNewComponent(() =>
+    Geometry({
+      shape: Polygon.rectangle(size),
+      position: position.add(size.divide(2)),
+    })
+  );
+
+  const body = useNewComponent(() =>
+    Physics.Body(geometry, {
+      isStatic: true,
+      isSensor: true,
+    })
+  );
+
+  if (callbacks.onStart || callbacks.onEnd) {
+    body.onCollision((info) => {
+      if (callbacks.onStart && info.kind === "start") {
+        callbacks.onStart.call(null, info);
+      } else if (callbacks.onEnd && info.kind === "end") {
+        callbacks.onEnd.call(null, info);
+      }
+    });
+  }
+}
+
+export function makeSensorBuilder(roomTopLeftOffset: Vector) {
+  return {
+    makeWall(
+      topLeftX: number,
+      topLeftY: number,
+      bottomRightX: number,
+      bottomRightY: number,
+      callbacks: SensorCallbacks
+    ) {
+      useChild(() =>
+        Sensor(
+          roomTopLeftOffset.addX(topLeftX).addYMutate(topLeftY),
+          new Vector(bottomRightX - topLeftX, bottomRightY - topLeftY),
+          callbacks
+        )
+      );
+    },
+  };
+}
