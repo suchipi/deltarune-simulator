956864ed25bba8c9cae6647e6342fbc762c94c93
Lily Skye <git@suchipi.tv>
2025-12-05 12:11:03 -0700

    this part was broken by draw order and, like, fuck it

--- src/Camera.ts
+++ src/Camera.ts
@@ -1,11 +1,8 @@
 import {
-  Polygon,
   ReadOnlyVector,
   useCanvasSize,
-  useInspectorHoverOutline,
   useRawDraw,
   useType,
-  Vector,
 } from "@hex-engine/2d";
 
 export function Camera(position: ReadOnlyVector) {
@@ -17,28 +14,6 @@ export function Camera(position: ReadOnlyVector) {
     context.translate(-position.x, -position.y);
     context.translate(canvasSize.x / 2, canvasSize.y / 2);
   });
-
-  if (process.env.NODE_ENV !== "production") {
-    // Polygon constructor changes point coords, so we have to enter them as
-    // positive and then adjust them afterwards :\
-    const debugShape = new Polygon([
-      new Vector(0, 0),
-      new Vector(160, 0),
-      new Vector(0, 80),
-      new Vector(160, 80),
-      new Vector(0, 0),
-      new Vector(0, 80),
-      new Vector(160, 80),
-      new Vector(160, 0),
-    ]);
-
-    for (const point of debugShape.points) {
-      point.x -= 80;
-      point.y -= 40;
-    }
-
-    useInspectorHoverOutline(() => debugShape);
-  }
 
   return {
     position,
