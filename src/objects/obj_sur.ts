37e5ef3da501bfefee642c98a38a83ce53954acf
Lily Skye <git@suchipi.tv>
2026-04-13 04:02:39 -0600

    add slope-up-left and slope-up-right

---
+++ src/objects/obj_sur.ts
@@ -0,0 +1,54 @@
+import {
+  Geometry,
+  Physics,
+  Polygon,
+  useNewComponent,
+  useType,
+  Vector,
+} from "@hex-engine/2d";
+import { RoomInstanceJson } from "../rooms/RoomJson";
+
+// 20x20 is the size of the game object sprite at
+// src/gamedata/chapter1/sprites/obj_sur.png
+const SIZE = new Vector(20, 20);
+
+export function obj_sur(instance: RoomInstanceJson) {
+  useType(obj_sur);
+
+  const size = SIZE.multiplyX(instance.scaleX).multiplyYMutate(instance.scaleY);
+
+  const points = [
+    new Vector(size.x, size.y),
+    new Vector(0, 0),
+    new Vector(0, size.y),
+  ];
+
+  // Need to counteract centroid offset from Polygon constructor (which is
+  // necessary for physics engine)
+  const centroid = points
+    .reduce((prev, curr) => prev.addMutate(curr), new Vector(0, 0))
+    .divideMutate(points.length);
+
+  const geometry = useNewComponent(() =>
+    Geometry({
+      shape: new Polygon(points),
+      position: new Vector(instance.x, instance.y).addMutate(
+        centroid.divide(2).addXMutate(size.x / 2),
+      ),
+      rotation: instance.rotation,
+    }),
+  );
+
+  const body = useNewComponent(() =>
+    Physics.Body(geometry, {
+      isStatic: true,
+    }),
+  );
+
+  return {
+    size,
+    geometry,
+    body,
+    centroid,
+  };
+}
